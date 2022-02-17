bed51d56c94b3b0c797cd92e1f637cde89e125ea
claramartins2002 <claramartins.cm57@gmail.com>
2022-02-17 14:28:03 -0300

    initial commit

---
+++ config-client/src/main/java/com/clara/configclient/HelloController.java
@@ -0,0 +1,28 @@
+package com.clara.configclient;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RefreshScope
+
+@RestController
+
+public class HelloController {
+
+    @Autowired
+
+    private Environment env;
+
+    @GetMapping("/hello")
+
+    public ResponseEntity<String> getHello(Model model) {
+        return new ResponseEntity<String>( env.getProperty("message"), HttpStatus.OK);
+    }
+
+}
